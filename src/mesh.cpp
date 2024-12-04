615ac628e3f1745bb959a9c81956dcd941e1d904
Robin <robin.van.den.eerenbeemd@gmail.com>
2024-12-04 16:00:20 +0000

    broken manifold detector

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,6 +1,7 @@
 #include "mesh.h"
 #include <cstdio>
 #include <filesystem>
+#include <CGAL/boost/graph/helpers.h>
 
 bool Mesh::loadMesh(std::string filename, Polygon_mesh &polygon)
 {
@@ -26,16 +27,60 @@ bool Mesh::validateMesh(Polygon_mesh &polygon)
     //https://doc.cgal.org/latest/Polygon_mesh_processing/index.html#Chapter_Manifoldness
 
     //check if geometry has non manifold vertex
-    for(vertex_descriptor v : vertices(polygon))
-    {
-        if(PMP::is_non_manifold_vertex(v, polygon))
-        {
-            std::cout << "vertex " << v << " is non-manifold" << std::endl;
-            return 0;
-        }
-    }
+  // for (vertex_descriptor v : vertices(polygon)) {
+  //     if (PMP::is_non_manifold_vertex(v, polygon)) {
+  //         std::cout << "Non-manifold vertex detected: " << v << std::endl;
+  //         return 0;
+  //     }
+  // }
 
-    //check if geometry has non manifold edges
+
+
+
+
+// Edge map to count edge occurrences
+  std::map<std::pair<int, int>, int> edge_map;
+
+  // Iterate over each face in the polygon mesh
+  for (const auto& f : faces(polygon)) {
+      // Get the first halfedge of the face
+      auto h = halfedge(f, polygon);
+      
+      // Traverse the halfedges of the face
+      do {
+          // Get the source and target vertices of the edge
+          auto v1 = source(h, polygon);  // Vertex 1
+          auto v2 = target(h, polygon);  // Vertex 2
+
+          // Create a pair representing the edge, sorted to avoid direction issues
+          std::pair<int, int> edge(std::min(v1, v2), std::max(v1, v2));
+          
+          // Increment the count of this edge in the map
+          edge_map[edge]++;
+          
+          // Move to the next halfedge
+          h = next(h, polygon);
+
+      } while (h != halfedge(f, polygon)); // Stop when we return to the first halfedge of the face
+  }
+
+
+  // After checking all faces, check for non-manifold edges
+  for (const auto& edge_pair : edge_map) {
+      std::cout<< "ENTERED FOR LOOP" << std::endl;
+      
+      if (edge_pair.second > 2) {
+          std::cout << "Non-manifold edge detected: "
+                    << edge_pair.first.first << " " << edge_pair.first.second << std::endl;
+          return 0;  // Non-manifold edge detected
+      }
+  }
+
+
+
+
+  
+  //check if geometry has non manifold edges
 
   // Check if faces are non-quad or non-triangle
   for (Polygon_mesh::Face_index f : faces(polygon)) {
--- tests/meshTests.cpp
+++ tests/meshTests.cpp
@@ -1,51 +1,46 @@
 #include <gtest/gtest.h>
 #include "mesh.h"
 
-TEST(Mesh, loadMesh)
-{
-    Mesh mesh;
-    Polygon_mesh validPolygon;   
-    Polygon_mesh invalidPolygon; 
+// TEST(Mesh, loadMesh)
+// {
+//     Mesh mesh;
+//     Polygon_mesh validPolygon;   
+//     Polygon_mesh invalidPolygon; 
 
-    EXPECT_TRUE(mesh.loadMesh("../test_files/deformed_sphere_export.obj", validPolygon));
-    EXPECT_FALSE(mesh.loadMesh("../test_files/doesntexist.obj", invalidPolygon));
+//     EXPECT_TRUE(mesh.loadMesh("../test_files/deformed_sphere_export.obj", validPolygon));
+//     EXPECT_FALSE(mesh.loadMesh("../test_files/doesntexist.obj", invalidPolygon));
 
-}
+// }
 
-TEST(Mesh, empty_file)
-{
-    Mesh mesh;
-    Polygon_mesh invalidPolygon;
-    EXPECT_FALSE(mesh.loadMesh("../test_files/empty.obj", invalidPolygon));
+// TEST(Mesh, empty_file)
+// {
+//     Mesh mesh;
+//     Polygon_mesh invalidPolygon;
+//     EXPECT_FALSE(mesh.loadMesh("../test_files/empty.obj", invalidPolygon));
 
-}
+// }
 
 TEST(Mesh, non_manifold_vertex_Mesh)
 {
     Mesh mesh;
     Polygon_mesh validPolygon;   
     Polygon_mesh invalidPolygon;
-    mesh.loadMesh("../test_files/deformed_sphere_export.obj", validPolygon);
+    //mesh.loadMesh("../test_files/deformed_sphere_export.obj", validPolygon);
     mesh.loadMesh("../test_files/manifold_export.obj", invalidPolygon);
     
-    EXPECT_TRUE(mesh.validateMesh(validPolygon));
-
-}
-
-//EXPECT_FALSE(mesh.validateMesh(invalidPolygon));
-
-
-TEST(Mesh, non_quad_Mesh)
-{
-    Mesh mesh;
-    Polygon_mesh validPolygon;   
-    Polygon_mesh invalidPolygon;
-    mesh.loadMesh("../test_files/deformed_sphere_export.obj", validPolygon);
-    mesh.loadMesh("../test_files/non_quad_export.obj", invalidPolygon);
-    
-    EXPECT_TRUE(mesh.validateMesh(validPolygon));
+    //EXPECT_TRUE(mesh.validateMesh(validPolygon));
     EXPECT_FALSE(mesh.validateMesh(invalidPolygon));
-
 }
 
+// TEST(Mesh, non_quad_Mesh)
+// {
+//     Mesh mesh;
+//     Polygon_mesh validPolygon;   
+//     Polygon_mesh invalidPolygon;
+//     mesh.loadMesh("../test_files/deformed_sphere_export.obj", validPolygon);
+//     mesh.loadMesh("../test_files/non_quad_export.obj", invalidPolygon);
+    
+//     EXPECT_TRUE(mesh.validateMesh(validPolygon));
+//     EXPECT_FALSE(mesh.validateMesh(invalidPolygon));
 
+// }
